0b6718e88cd2ad1ef5460b6686ae3504e9004253
zap-mueda <m_ueda@zappallas.com>
2018-06-15 18:45:44 +0900

    素数シェルスクリプト修正とテストシェルスクリプト作成
    ・素数シェルスクリプトは引数で数値を受け取るように修正
    ・テストシェルスクリプト作成

---
+++ 18445013-test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#引数が数値かどうかチェック
+echo "引数は数値を入力してください" > $$-temptest
+./18445013.sh number > $$-temptest-ans
+diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || echo "「引数は数値を入力してください」の機能が間違えています。"
+
+#引数の数チェック
+echo "引数の数は1つにしてください" > $$-temptest
+./18445013.sh 1 2 > $$-temptest-ans
+diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || echo "「引数の数は1つにしてください」の機能が間違えています。"
+
+#引数が数値かどうかチェック
+echo "数値は2以上100以下で入力してください" > $$-temptest
+./18445013.sh 1 > $$-temptest-ans
+diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || echo "「数値は2以上100以下で入力してください」の機能が間違えています。"
+
+#引数が数値かどうかチェック
+echo "数値は2以上100以下で入力してください" > $$-temptest
+./18445013.sh 1 > $$-temptest-ans
+diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || echo "「数値は2以上100以下で入力してください」の機能が間違えています。"
+
+#素数の判定がちゃんとできているかチェック
+num=3
+echo $num "は素数です" > $$-temptest
+./18445013.sh $num > $$-temptest-ans
+diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || echo "「素数判定」が間違えています"
+
+num=5
+echo $num "は素数ではありません" > $$-temptest
+./18445013.sh $num > $$-temptest-ans
+diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || echo "「素数判定」が間違えています"
+
+
+rm -f ./$$-*
--- 18445013.sh
+++ 18445013.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
+entryNum=$1
 
-echo "2から100までの数値を入力してください"
 
-#数値入力
-while true; do
-  read entryNum
-  if [ "$entryNum" -lt "2" -o "$entryNum" -gt "100" ]; then
-    echo "数値は2以上100以下で入力してください"
-  else
-    break
-  fi
-done
+#数値かどうかチェック
+expr $1 + 0 > /dev/null 2>&1
+if [ $? -ge 2 ]; then
+  echo "引数は数値を入力してください"
+  exit 1
+fi
+
+#引数の数チェック
+if [ $# -ne 1 ]; then 
+  echo "引数の数は1つにしてください"
+  exit 1
+fi
+
+#数値の入力チェック
+if [ $entryNum -lt 2 -o $entryNum -gt 100 ]; then
+  echo "数値は2以上100以下で入力してください"
+  exit 1
+fi
+
 
 #素数判定
 flag=0
 if [ $entryNum -gt 2 ]; then
   for num in `seq 2 $((entryNum - 1))`
   do
-      if [ $((entryNum % num)) -eq "0" ]; then
-	flag=1
-       break
-      fi       
+    if [ $((entryNum % num)) -eq "0" ]; then
+      flag=1
+      break
+    fi       
   done
 fi
 
 #結果表示
-if [ "$flag" -eq "1" ]; then
-  echo "$entryNum は素数ではありません"
+if [ $flag -eq 1 ]; then
+  echo $entryNum "は素数ではありません"
 else
-  echo "$entryNum は素数です"
+  echo $entryNum "は素数です"
 fi
