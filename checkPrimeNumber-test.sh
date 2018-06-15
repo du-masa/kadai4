3093910d89812351651e339a08d2a4af6f2c02f8
zap-mueda <m_ueda@zappallas.com>
2018-06-16 07:49:52 +0900

    テストないのファイル名変更

--- checkPrimeNumber-test.sh
+++ checkPrimeNumber-test.sh
@@ -8,28 +8,28 @@ ERROR_EXIT () {
 
 #引数が数値かどうかチェック
 echo "引数は数値を入力してください" > $$-temptest
-./18445013.sh number > $$-temptest-ans
+./checkPrimeNumber.sh number > $$-temptest-ans
 diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || ERROR_EXIT "「引数は数値を入力してください」の機能が間違えています。"
 
 #引数の数チェック
 echo "引数の数は1つにしてください" > $$-temptest
-./18445013.sh 1 2 > $$-temptest-ans
+./checkPrimeNumber.sh 1 2 > $$-temptest-ans
 diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || ERROR_EXIT "「引数の数は1つにしてください」の機能が間違えています。"
 
 #引数が数値かどうかチェック
 echo "数値は2以上100以下で入力してください" > $$-temptest
-./18445013.sh 1 > $$-temptest-ans
+./checkPrimeNumber.sh 1 > $$-temptest-ans
 diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || ERROR_EXIT "「数値は2以上100以下で入力してください」の機能が間違えています。"
 
 #素数の判定がちゃんとできているかチェック
 num=3
 echo $num "は素数です" > $$-temptest
-./18445013.sh $num > $$-temptest-ans
+./checkPrimeNumber.sh $num > $$-temptest-ans
 diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || ERROR_EXIT "「素数である」判定が間違えています"
 
 num=4
 echo $num "は素数ではありません" > $$-temptest
-./18445013.sh $num > $$-temptest-ans
+./checkPrimeNumber.sh $num > $$-temptest-ans
 diff $$-temptest-ans $$-temptest > /dev/null 2>&1 || ERROR_EXIT "「素数でない」判定が間違えています"
 
 
